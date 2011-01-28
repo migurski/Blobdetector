41b038ffc8ff9e37e278bfbc928686c54063f718
Michal Migurski <mike@stamen.com>
2011-01-27 17:47:45 -0800

    Moved atkinson.py to blobdetector.py

--- blobdetector.py
+++ blobdetector.py
@@ -1,8 +1,8 @@
 """
-Test dither() with a small 4x4 square
+Test detect() with a small 4x4 square
 >>> input = '\xee\xee\x11\x11\xee\x11\x11\x11\x11\x11\xee\xee\x11\x11\xee\xee'
->>> output = dither(PIL.Image.fromstring('L', (4, 4), input))
->>> output.tostring()
+>>> output = detect(PIL.Image.fromstring('L', (4, 4), input))
+>>> output
 '\\x01\\x01\\x00\\x00\\x01\\x00\\x00\\x00\\x00\\x00\\x02\\x02\\x00\\x00\\x02\\x02'
 >>> input
 '\\xee\\xee\\x11\\x11\\xee\\x11\\x11\\x11\\x11\\x11\\xee\\xee\\x11\\x11\\xee\\xee'
@@ -11,8 +11,8 @@ Test dither() with a small 4x4 square
 import blobs
 import PIL.Image
 
-def dither(i):
-    """ Take an instance of single-channel PIL.Image, dither and return
+def detect(i):
+    """ Take an instance of single-channel PIL.Image, detect blobs and return
     """
     assert i.mode == 'L'
     s = blobs.detect(i.size[0], i.size[1], i.tostring())
