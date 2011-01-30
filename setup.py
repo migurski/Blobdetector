1c198b8a725bb86697d3e6d1e8f222fff5fa1d09
Michal Migurski <mike@stamen.com>
2011-01-29 17:14:05 -0800

    Updated setup script with new package info; doesn't yet seem to work

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ from distutils.core import setup, Extension
 
 blobs = Extension('_blobs', sources=['blobs.cpp'])
 
-setup (name = 'BlobDetector',
-       version = '0.9.0',
-       description = 'A small package that detects and categorizes blobs in images.',
-       author='Michal Migurski',
-       author_email='mike@stamen.com',
-       ext_modules = [blobs])
+setup(name = 'BlobDetector',
+      version = '0.9.0',
+      description = 'A small package that detects and categorizes blobs in images.',
+      author = 'Michal Migurski',
+      author_email = 'mike@stamen.com',
+      url = 'https://github.com/migurski/Blobdetector',
+      packages = ['BlobDetector'],
+      ext_modules = [blobs],
+      license = 'BSD')
